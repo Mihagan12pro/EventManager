7ba942da7ae694d49bd42366053bb3cb03728813
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-02-28 15:24:35 +0300

    feat: add Event class

---
+++ src/EventManager/DomainModels/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace EventManager.DomainModels
+{
+    public class Event
+    {
+        public required Guid Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public required DateTime StartAt { get; set; }
+
+        public required DateTime EndAt { get; set; }
+    }
+}
