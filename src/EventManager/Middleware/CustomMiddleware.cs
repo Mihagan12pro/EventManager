47e7e4c9a4c977f65b90c63a429393803b6af297
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-03-16 12:36:01 +0300

    feat: add CustomMiddleware class

---
+++ src/EventManager/Middleware/CustomMiddleware.cs
@@ -0,0 +1,14 @@
+﻿namespace EventManager.Middleware
+{
+    public abstract class CustomMiddleware
+    {
+        protected readonly RequestDelegate next;
+
+        public abstract Task InvokeAsync(HttpContext context);
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+    }
+}
