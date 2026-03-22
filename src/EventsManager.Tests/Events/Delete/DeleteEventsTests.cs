e8d9c8b500e1d6ed07912304274b7995af52b0ab
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-03-23 00:47:40 +0300

    chore: add normal deleting tests

---
+++ src/EventsManager.Tests/Events/Delete/DeleteEventsTests.cs
@@ -0,0 +1,35 @@
+﻿using EventManager.DTOs.Events;
+using EventManager.Services.Events;
+
+namespace EventsManager.Tests.Events.Delete
+{
+    [Collection("Delete events collection")]
+    public partial class DeleteEventsTests
+    {
+        [Theory]
+        [MemberData(nameof(AddEventsForDeleting))]
+        public async Task Test_Basic_Deleting(NewEventDto eventDto)
+        {
+            var eventsService = new EventsService();
+
+            Guid id = (await eventsService.AddNew(eventDto)).Value;
+
+            var test1 = await eventsService.Delete(id);
+            var test2 = await eventsService.Delete(id);
+
+            Assert.True(test1.IsSuccess);
+            Assert.False(test2.IsSuccess);
+        }
+
+        [Theory]
+        [MemberData(nameof(AddNotExistsEventsForDeleting))]
+        public async Task Test_Not_Exists_Deleting(Guid id)
+        {
+            var eventsService = new EventsService();
+
+            var test = await eventsService.Delete(id);
+
+            Assert.False(test.IsSuccess);
+        }
+    }
+}
