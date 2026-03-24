40245d46a417c103dd6cc4035410e153b5fd6568
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-03-24 10:02:52 +0300

    chore: new put tests

--- src/EventsManager.Tests/Events/Put/PutEventsTests.cs
+++ src/EventsManager.Tests/Events/Put/PutEventsTests.cs
@@ -34,92 +34,66 @@ namespace EventsManager.Tests.Events.Put
             var exception = await Assert.ThrowsAsync<BadRequestException>(() => eventsService.UpdateByPut(id, eventDto));
         }
 
-        //[Fact]
-        //public async Task Test_Putting_With_Error_404()
-        //{
-        //    var eventsService = new EventsService();
-        //    Guid id = Guid.NewGuid();
+        [Fact]
+        public async Task Test_Putting_With_Error_404()
+        {
+            var eventsService = new EventsService();
+            Guid id = Guid.NewGuid();
 
-        //    NewEventDto eventDto = new NewEventDto(
-        //        string.Empty,
-        //        DateTime.Now.AddDays(1),
-        //        DateTime.Now.AddDays(2)
-        //    );
+            NewEventDto eventDto = new NewEventDto(
+                string.Empty,
+                DateTime.Now.AddDays(1),
+                DateTime.Now.AddDays(2)
+            );
 
-        //    var result = await eventsService.UpdateByPut(id, eventDto);
+            var result = await Assert.ThrowsAsync<NotFoundException>(() => eventsService.UpdateByPut(id, eventDto));
+        }
 
-        //    Assert.False(result.IsSuccess);
-        //    Assert.Equal(404, result.Error.StatusCode);
-        //}
+        [Theory]
+        [MemberData(nameof(PutData))]
+        public async Task Test_Putting(int index, NewEventDto eventDto)
+        {
+            DateTime dateTime = new DateTime(new DateOnly(2027, 5, 1), new TimeOnly(20, 20)).AddYears(2);
+            var eventsService = new EventsService();
 
-        //[Theory]
-        //[MemberData(nameof(PutData))]
-        //public async Task Test_Putting(int index, NewEventDto eventDto)
-        //{
-        //    DateTime dateTime = new DateTime(new DateOnly(2027, 5, 1), new TimeOnly(20, 20)).AddYears(2);
-        //    var eventsService = new EventsService();
+            await eventsService.AddNew(
+                 new NewEventDto(
+                     "Юбилей",
+                     dateTime.AddDays(1),
+                     dateTime.AddDays(2))
+                 );
 
-        //    await eventsService.AddNew(
-        //         new NewEventDto(
-        //             "Юбилей",
-        //             dateTime.AddDays(1),
-        //             dateTime.AddDays(2))
-        //         );
+            Event oldModel = (await eventsService.GetEvents(
+                null,
+                new PaginationDto(),
+                new DateRange(
+                     null,
+                     false,
+                     null,
+                     false)
+                )).Events.First();
 
-        //    await eventsService.AddNew(
-        //        new NewEventDto(
-        //            "Юбилей",
-        //            dateTime.AddDays(1),
-        //            dateTime.AddDays(2))
-        //        );
+            DateTime start = oldModel.StartAt;
+            DateTime end = oldModel.EndAt;
 
-        //    await eventsService.AddNew(
-        //        new NewEventDto(
-        //            "Юбилей",
-        //            dateTime.AddDays(2),
-        //            dateTime.AddDays(3))
-        //        );
+            string title = oldModel.Title;
+            string description = oldModel.Description;
 
-        //    await eventsService.AddNew(
-        //        new NewEventDto(
-        //            "Корпоратив",
-        //            dateTime.AddDays(2),
-        //            dateTime.AddDays(3))
-        //        );
+            var result = (await eventsService.UpdateByPut(oldModel.Id, eventDto));
+            Event putModel = (await eventsService.GetEvents(
+                null,
+                new PaginationDto(),
+                new DateRange(
+                     null,
+                     false,
+                     null,
+                     false)
+                )).Events.First();
 
-        //    Event oldModel = (await eventsService.GetEvents(
-        //        null,
-        //        new PaginationDto(),
-        //        new DateRange(
-        //             null,
-        //             false,
-        //             null,
-        //             false)
-        //        )).Events.First();
-
-        //    DateTime start = oldModel.StartAt;
-        //    DateTime end = oldModel.EndAt;
-
-        //    string title = oldModel.Title;
-        //    string description = oldModel.Description;
-
-        //    var result = (await eventsService.UpdateByPut(oldModel.Id, eventDto));
-        //    Event putModel = (await eventsService.GetEvents(
-        //        null,
-        //        new PaginationDto(),
-        //        new DateRange(
-        //             null,
-        //             false,
-        //             null,
-        //             false)
-        //        )).Events.First();
-
-        //    Assert.True(result.IsSuccess);
-
-        //    Assert.False(title == putModel.Title);
-        //    Assert.False(description == putModel.Description);
-        //    Assert.False(start == putModel.StartAt);
-        //    Assert.False(end == putModel.EndAt);
-        //}
+            Assert.False(title == putModel.Title);
+            Assert.False(description == putModel.Description);
+            Assert.False(start == putModel.StartAt);
+            Assert.False(end == putModel.EndAt);
+        }
     }
 }
