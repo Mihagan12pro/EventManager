c09446ae9ae0b85be5c35e59a325cb0ded7a3beb
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-03-30 16:00:57 +0300

    feat: add the InternalsVisibleTo attribute to namespace EventManager.Services.Background.Bookings

--- src/EventsManager.Tests/Booking/Create/CreateBookCtor.cs
+++ src/EventsManager.Tests/Booking/Create/CreateBookCtor.cs
@@ -2,11 +2,14 @@
 {
     public partial class CreateBookTests
     {
+        private readonly Type _bookingHandlingServiceType;
         private readonly Type _bookingsServiceType;
         private readonly Type _eventsServiceType;
 
         public CreateBookTests()
         {
+            _bookingHandlingServiceType = Type.GetType("EventManager.Services.Background.Bookings.BookingHandlingService, EventManager.Services");  
+
             _eventsServiceType = Type.GetType("EventManager.Services.Events.EventsService, EventManager.Services");
             _bookingsServiceType = Type.GetType("EventManager.Services.Bookings.BookingsService, EventManager.Services");
         }
--- src/EventsManager.Tests/Booking/Create/CreateBookTests.cs
+++ src/EventsManager.Tests/Booking/Create/CreateBookTests.cs
@@ -3,6 +3,13 @@ using EventManager.DTOs.Bookings;
 using EventManager.DTOs.Events;
 using EventManager.Services.Bookings;
 using EventManager.Services.Events;
+using EventManager.Services.Exceptions;
+using Moq;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using EventManager.Queues.Queues.Booking;
+using EventManager.Services.Background.Bookings;
 
 namespace EventManager.Tests.Booking.Create
 {
@@ -22,6 +29,31 @@ namespace EventManager.Tests.Booking.Create
             var booking = await bookingsService.GetBookingByIdAsync(acceptedDto.Id);
 
             Assert.Equal(BookingStatus.Pending, booking.Status);
+        }
+
+        [Fact]
+        public async Task Test_Booking_Creating_Exceptions()
+        {
+            IEventsService eventsService = (IEventsService)Activator.CreateInstance(_eventsServiceType);
+            IBookingsService bookingsService = (IBookingsService)Activator.CreateInstance(_bookingsServiceType, eventsService);
+
+            Guid id = Guid.Empty;
+
+            await Assert.ThrowsAsync<NotFoundException>( () => bookingsService.CreateBookingAsync(id) );
+        }
+
+        [Theory]
+        [MemberData(nameof(AddEvents))]
+        public async Task Test_Background_Service(NewEventDto eventDto)
+        {
+            IEventsService eventsService = (IEventsService)Activator.CreateInstance(_eventsServiceType);
+            IBookingsService bookingsService = (IBookingsService)Activator.CreateInstance(_bookingsServiceType, eventsService);
+
+            var queue = new Mock<IBookingTaskQueue>();
+            var factory = new Mock<IServiceScopeFactory>();
+            var logger = new Mock<ILogger<BookingHandlingService>>();
+
+            BookingHandlingService bookingHandlingService = new BookingHandlingService(queue.Object, factory.Object, logger.Object);
         }
     }
 }
