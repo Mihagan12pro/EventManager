d0778f16a97dbf2baa300799f6e090b7a5e4b8ff
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-04-01 18:40:35 +0300

    chore: new test - Test_Adding_Two_Books_For_Event

--- src/EventsManager.Tests/Booking/Create/CreateBookTests.cs
+++ src/EventsManager.Tests/Booking/Create/CreateBookTests.cs
@@ -1,20 +1,31 @@
-﻿using EventManager.Domain.Bookings.Enums;
+﻿using EventManager.Domain.Bookings;
+using EventManager.Domain.Bookings.Enums;
 using EventManager.DTOs.Bookings;
 using EventManager.DTOs.Events;
 using EventManager.Services.Bookings;
 using EventManager.Services.Events;
 using EventManager.Services.Exceptions;
-using Moq;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using EventManager.Queues.Queues.Booking;
-using EventManager.Services.Background.Bookings;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 
 namespace EventManager.Tests.Booking.Create
 {
     public partial class CreateBookTests
     {
+        [Theory]
+        [MemberData(nameof(AddEvents))]
+        public async Task Test_Adding_Two_Books_For_Event(NewEventDto eventDto)
+        {
+            IEventsService eventsService = (IEventsService)Activator.CreateInstance(_eventsServiceType);
+            IBookingsService bookingsService = (IBookingsService)Activator.CreateInstance(_bookingsServiceType, eventsService);
+
+            Guid eventId = await eventsService.AddNewAsync(eventDto);
+
+            BookingAcceptedDto accepted1 = await bookingsService.CreateBookingAsync(eventId);
+            BookingAcceptedDto accepted2 = await bookingsService.CreateBookingAsync(eventId);
+
+            Assert.False(accepted1.Id == accepted2.Id);
+        }
+
         [Theory]
         [MemberData(nameof(AddEvents))]
         public async Task Test_Booking_Pending_Status(NewEventDto eventDto)
@@ -40,20 +51,6 @@ namespace EventManager.Tests.Booking.Create
             Guid id = Guid.Empty;
 
             await Assert.ThrowsAsync<NotFoundException>( () => bookingsService.CreateBookingAsync(id) );
-        }
-
-        [Theory]
-        [MemberData(nameof(AddEvents))]
-        public async Task Test_Background_Service(NewEventDto eventDto)
-        {
-            IEventsService eventsService = (IEventsService)Activator.CreateInstance(_eventsServiceType);
-            IBookingsService bookingsService = (IBookingsService)Activator.CreateInstance(_bookingsServiceType, eventsService);
-
-            var queue = new Mock<IBookingTaskQueue>();
-            var factory = new Mock<IServiceScopeFactory>();
-            var logger = new Mock<ILogger<BookingHandlingService>>();
-
-            BookingHandlingService bookingHandlingService = new BookingHandlingService(queue.Object, factory.Object, logger.Object);
         }
     }
 }
