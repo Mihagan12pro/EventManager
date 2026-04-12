f018965420c591154b005d2b7f80045de819ae1b
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-04-12 19:18:45 +0300

    refactor: make ControllerExtensions.ErrorToActionResult less complex

--- src/EventManager/Extensions/ControllerExtensions.cs
+++ src/EventManager/Extensions/ControllerExtensions.cs
@@ -6,41 +6,7 @@ namespace EventManager.Extensions
 {
     public static class ControllerExtensions
     {
-        public static IActionResult ErrorToActionResult(
-            this ControllerBase controllerBase, 
-            HttpError error)
-        {
-            IActionResult result;
-            string message = error.Message;
-
-            switch (error.StatusCode)
-            {
-                case HttpStatusCode.BadRequest:
-                    {
-                        result = controllerBase.BadRequest(message);
-                        break;
-                    }
-
-                case HttpStatusCode.NotFound:
-                    {
-                        result = controllerBase.NotFound(message);
-                        break;
-                    }
-
-                case HttpStatusCode.InternalServerError:
-                    {
-                        result = controllerBase.StatusCode(500, message);
-                        break;
-                    }
-
-                default:
-                    {
-                        result = controllerBase.StatusCode((int)error.StatusCode, message);
-                        break;
-                    }
-            }
-
-            return result;
-        }
+        public static IActionResult ErrorToActionResult(this ControllerBase controllerBase, HttpError error)
+            => controllerBase.StatusCode((int)error.StatusCode, error.Message);
     }
 }
