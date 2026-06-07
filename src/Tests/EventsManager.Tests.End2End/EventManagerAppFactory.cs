0abe033ba076939097f4fb64872dd6b9c819fea9
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-06-07 18:42:58 +0300

    chore: add E2E tests

---
+++ src/Tests/EventsManager.Tests.End2End/EventManagerAppFactory.cs
@@ -0,0 +1,32 @@
+﻿using EventManager.Infrastructure.PostgreSQL.DbContexts;
+using EventManager.Tests.Abstractions;
+using Microsoft.AspNetCore.Hosting;
+using EventManager.Handlers;
+using EventManager.Infrastructure.PostgreSQL;
+using EventManager.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsManager.Tests.End2End
+{
+    public class EventManagerAppFactory<TEntryPoint>
+         : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddDbContext<AppDbContextBase, DockerAppDbContext>();
+
+                services.AddRepositories();
+                services.AddBackgroundServices();
+                services.AddHandlers();
+
+                services.AddLogging();
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
---
+++ src/Tests/EventsManager.Tests.End2End/Events/EventsTests.cs
@@ -0,0 +1,57 @@
+﻿using EventManager.DTOs.Events;
+using EventManager.Tests.Abstractions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace EventsManager.Tests.End2End.Events
+{
+    public class EventsTests : E2ETests
+    {
+        public EventsTests(EventManagerAppFactory<Program> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Test_AddingNew()
+        {
+            await ResetDatabaseAsync();
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            NewEventDto newEvent = new NewEventDto("Birthday", DateTime.UtcNow.AddYears(1), DateTime.UtcNow.AddYears(1).AddDays(1), 10);
+
+            var response = await httpClient.PostAsJsonAsync(@"events\", newEvent, cts.Token);
+
+            Assert.Equal(response.StatusCode, System.Net.HttpStatusCode.Created);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Guid.TryParse(responseBody.Trim('"'), out Guid id);
+
+            Assert.NotEqual(Guid.Empty, id);
+        }
+
+        [Fact]
+        public async Task Test_GetDeletedEvent()
+        {
+            await ResetDatabaseAsync();
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            NewEventDto newEvent = new NewEventDto("Birthday", DateTime.UtcNow.AddYears(1), DateTime.UtcNow.AddYears(1).AddDays(1), 10);
+
+            var postResponse = await httpClient.PostAsJsonAsync(@"events\", newEvent, cts.Token);
+
+            string responseBody = (await postResponse.Content.ReadAsStringAsync()).Trim('"');
+
+            Guid.TryParse(responseBody.Trim('"'), out Guid id);
+
+            var deleteResponse = await httpClient.DeleteAsync(@$"events\{id}");
+
+            Assert.Equal(deleteResponse.StatusCode, HttpStatusCode.OK);
+
+            var getResult = await httpClient.GetAsync($@"events\{id}");
+
+            Assert.Equal(getResult.StatusCode, HttpStatusCode.NotFound);
+        }
+    }
+}
