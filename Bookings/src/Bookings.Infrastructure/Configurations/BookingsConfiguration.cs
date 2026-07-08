f07063d3b055032c3d423b8361b5e4e697ea8560
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-07-08 17:41:28 +0300

    feat: make eventId index

---
+++ Bookings/src/Bookings.Infrastructure/Configurations/BookingsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Bookings.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookings.Infrastructure.Configurations
+{
+    internal class BookingsConfiguration 
+        : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.HasIndex(b => b.EventId);
+        }
+    }
+}
---
+++ Bookings/src/Bookings.Infrastructure/Migrations/20260708143853_Add_EventIdIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookings.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_EventIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_EventId",
+                table: "Bookings",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_EventId",
+                table: "Bookings");
+        }
+    }
+}
