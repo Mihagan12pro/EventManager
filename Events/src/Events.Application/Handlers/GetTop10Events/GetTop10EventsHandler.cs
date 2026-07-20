e348c9bf65e3cbc9a2d383bbad396e4c6c25e50e
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-07-20 11:17:18 +0300

    feat: modify redis container

--- Events/src/Events.Application/Handlers/GetTop10Events/GetTop10EventsHandler.cs
+++ Events/src/Events.Application/Handlers/GetTop10Events/GetTop10EventsHandler.cs
@@ -1,18 +1,21 @@
 ﻿using Events.Application.Dtos;
-using Events.Application.Repositories.Events;
+using Events.Application.Repositories.Cache;
 using Shared.Objects.Interfaces;
 
 namespace Events.Application.Handlers.GetTop10Events
 {
     internal class GetTop10EventsHandler : ICommandHandler<IEnumerable<GetEventDto>, GetTop10EventsCommand>
     {
-        private readonly IReadEventsRepository _eventsRepository;
+        private readonly ICacheRepository _cacheRepository;
 
         public async Task<IEnumerable<GetEventDto>> HandleAsync(
             GetTop10EventsCommand command,
             CancellationToken cancellationToken)
         {
-            var events = await _eventsRepository.GetMostPopularAsync(10, cancellationToken);
+            var events = await _cacheRepository.GetMostPopularAsync(10, cancellationToken);
+
+            if (events == null)
+                return null;
 
             return events.Select(e => new GetEventDto(
                 e.Id, 
@@ -25,9 +28,9 @@ namespace Events.Application.Handlers.GetTop10Events
             );
         }
 
-        public GetTop10EventsHandler(IReadEventsRepository eventsRepository)
+        public GetTop10EventsHandler(ICacheRepository cacheRepository)
         {
-            _eventsRepository = eventsRepository;
+            _cacheRepository = cacheRepository;
         }
     }
 }
